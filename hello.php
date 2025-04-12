57ea63a676aa6cefc0cf21c91d5c2251923ea1da
Warlana1451 <anwar.mln79@gmail.com>
2025-04-12 10:46:48 +0700

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    function salam($nama='Teman!'){
+        echo "<h2> Yoo Selamat Datang, $nama </h2>";
+    }
+    ?>
+
+    <h1>STT TERPADU NURUL FIKRI</h1>
+
+    <?php 
+    salam("Anwar");
+    ?>
+</body>
+</html>
